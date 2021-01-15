6f96419d29e8641e2b8c75019262ee7bc7c04681
Mike Gordillo <mikegordillotattoo@gmail.com>
2021-01-15 12:08:21 -0500

    wrote functions to script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+function startGame(){
+
+}
+
+function nextQuestion(){
+
+}
+
+function setNextQuestion() {
+}
+
+function showQuestion(question){
+
+}
+
+function resetState() {
+
+}
+
+function selectAnswer(e){
+
+}
+function setStatusClass
+
+
+function clearStatusClass
