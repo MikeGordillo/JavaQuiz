69ff47c963ba369105278a09d0f4eb63346b50ef
Mike Gordillo <mikegordillotattoo@gmail.com>
2021-01-16 01:04:35 -0500

    added second.html

--- script.js
+++ script.js
@@ -22,6 +22,8 @@ window.onload = function () {
     startTimer(fiveMinutes, display);
 };
 
+
+
 const startButton = document.getElementById('start-btn')
 const nextButton = document.getElementById('next-btn')
 const questionContainerElement = document.getElementById('question-container')
